868ca1120502db843df3696db3d5a2e31ca7077c
rayphee <r.mueen@gmail.com>
2017-03-27 19:04:17 -0400

    IDK

---
+++ OutputInstructions/Keil 8051 v9.53 - Debug/src/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c 
+
+OBJS += \
+./src/main.OBJ 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.OBJ: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Compiler'
+	wine "/Applications/Simplicity Studio.app/Contents/Eclipse/developer/toolchains/keil_8051/9.53/BIN/C51" "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/main.OBJ: /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.0.3/Device/EFM8BB3/inc/SI_EFM8BB3_Register_Enums.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.0.3/Device/EFM8BB3/inc/SI_EFM8BB3_Defs.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.0.3/Device/shared/si8051Base/si_toolchain.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.0.3/Device/shared/si8051Base/stdint.h /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/8051/v4.0.3/Device/shared/si8051Base/stdbool.h
+
+
--- OutputInstructions/src/main.c
+++ OutputInstructions/src/main.c
@@ -17,7 +17,42 @@
 // Simplicity Launcher and click on "Data Sheet".
 //
 //-----------------------------------------------------------------------------
+void writeOpcodePin0(bool);
+void writeOpcodePin1(bool);
+void writeParamPin0(bool);
+void writeParamPin1(bool);
+void writeParamPin2(bool);
+void writeParamPin3(bool);
+void writeClockPin(bool);
+void delayBS(int);
 
+void issueInstruction(char opcode, char param){
+	delayBS(4500);
+	writeClockPin(false);
+	delayBS(2250);
+	writeOpcodePin0(opcode & 1);
+	writeOpcodePin1(opcode & 2);
+	writeParamPin0(param & 1);
+	writeParamPin1(param & 2);
+	writeParamPin2(param & 4);
+	writeParamPin3(param & 8);
+	delayBS(2250);
+	writeClockPin(true);
+}
+
+// What is your ISA?
+// 0x00 - GROUND
+// 0x01 - OR
+// 0x02 - AND
+// 0x03 - ADD
+
+void main(){
+	issueInstruction(0x00, 0x00); // acc now 0x00
+	issueInstruction(0x03, 0x01); // acc now 0x01
+	issueInstruction(0x01, 0x02); // acc now 0x11
+	issueInstruction(0x02, 0x02); // acc now 0x10
+	issueInstruction(0x03, 0x01); // acc now 0x11 (again);
+}
 int main (void)
 {
 	while (1) {}                             // Spin forever
